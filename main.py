7d8c3451e0cbb59ee3196caaf85ea29506d45147
breakhuman <97409934+breakhuman@users.noreply.github.com>
2022-01-25 18:52:23 +0900

    second

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # module or another variable, defines list
 from urllib import response
 from tgkey import id
-import telegram
+from telegram import *
 from telegram.ext import *
 import bornbot as b
 
 # running print
 print('동작중....')
+
+#####################################################################
+
+#####################################################################
 
 # def list
 def start_command(update, context):
@@ -35,7 +39,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error)
     
-    updater.start_polling(10)
+    updater.start_polling()
     updater.idle()
 
 main()
